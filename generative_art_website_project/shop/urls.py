6af02d1ceb40e703408af1987ac2351098755c17
Raunit <raunitd.it.17@nsit.net.in>
2020-10-22 16:44:35 +0530

    F; font and content changes

--- generative_art_website_project/shop/urls.py
+++ generative_art_website_project/shop/urls.py
@@ -18,8 +18,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.shop, name='shop'),
-    path('cart/',views.cart, name='cart'),
-    path('checkout/',views.checkout, name='checkout'),
-    
+    path('', views.shop, name='shop'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+
 ]
--- generative_art_website_project/shop/views.py
+++ generative_art_website_project/shop/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render
 from .models import *
+
+
 # Create your views here.
 
 def shop(request):
     products = Product.objects.all()
     print(products)
-    context = {'products':products}
-    return render(request,'shop/shop.html',context)
+    context = {'products': products}
+    return render(request, 'shop/shop.html', context)
+
 
 def cart(request):
-
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
     else:
         items = []
-        order = {'get_cart_total':0}
-    context = {'items':items, 'order':order}
-    return render(request,'shop/cart.html',context)
+        order = {'get_cart_total': 0}
+    context = {'items': items, 'order': order}
+    return render(request, 'shop/cart.html', context)
+
 
 def checkout(request):
     if request.user.is_authenticated:
@@ -27,6 +30,6 @@ def checkout(request):
         items = order.orderitem_set.all()
     else:
         items = []
-        order = {'get_cart_total':0}
-    context = {'items':items, 'order':order}
-    return render(request,'shop/checkout.html',context)
+        order = {'get_cart_total': 0}
+    context = {'items': items, 'order': order}
+    return render(request, 'shop/checkout.html', context)

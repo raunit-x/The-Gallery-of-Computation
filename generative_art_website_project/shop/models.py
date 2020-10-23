3635687bf238dd4662b0e774775857b935ae2902
Raunit <raunitd.it.17@nsit.net.in>
2020-10-23 11:01:29 +0530

    refactored, added image for cart

--- generative_art_website_project/shop/models.py
+++ generative_art_website_project/shop/models.py
@@ -41,14 +41,12 @@ class Order(models.Model):
 
     @property
     def get_cart_total(self):
-        orderitems = self.orderitem_set.all()
-        total = sum([item.get_total for item in orderitems])
+        order_items = self.orderitem_set.all()
+        for order in order_items:
+            print(order.product)
+        total = sum([item.get_price for item in order_items])
         return total
 
-    # def get_cart_items(self):
-    #     orderitems = self.orderitem_set.all()
-    #     total = sum([item.quantity for item in orderitems])
-    #     return total  
     def __str__(self):
         return str(self.id)
 
@@ -56,13 +54,12 @@ class Order(models.Model):
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
-    quantity = models.IntegerField(default=0, null=True, blank=True)
+    # quantity = models.IntegerField(default=0, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
 
     @property
-    def get_total(self):
-        total = self.product.price * self.quantity
-        return total
+    def get_price(self):
+        return self.product.price
 
 
 class ShippingAddress(models.Model):

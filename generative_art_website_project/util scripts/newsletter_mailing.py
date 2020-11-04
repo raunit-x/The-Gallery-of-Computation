afdd46271fc1b1afb88a440a623dfa61e09f43b0
Raunit <raunitd.it.17@nsit.net.in>
2020-11-04 11:14:25 +0530

    basic mailing script working

--- generative_art_website_project/util scripts/newsletter_mailing.py
+++ generative_art_website_project/util scripts/newsletter_mailing.py
@@ -1,12 +1,32 @@
-from mailer import Mailer
-from mailer import Message
+import email, smtplib, ssl
 
-message = Message(From="raunitd.it.17@nsit.net.in",
-                  To="parullakhotia@gmail.com")
-message.Subject = "An HTML Email"
-message.Html = """<p>Hi!<br>
-   How are you?<br>
-   Here is the <a href="http://www.python.org">link</a> you wanted.</p>"""
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-sender = Mailer('smtp.example.com')
-sender.send(message)
+subject = "An email with attachment from Python"
+body = "This is an email with attachment sent from Python"
+sender_email = "raunitxgenerativeart@gmail.com"
+receiver_email = "raunit88@gmail.com"
+password = input("Type your password and press enter:")
+
+# Create a multipart message and set headers
+message = MIMEMultipart()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+message["Bcc"] = receiver_email  # Recommended for mass emails
+
+# Add body to email
+message.attach(MIMEText(body, "plain"))
+
+text = message.as_string()
+
+# Log in to server using secure context and send email
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.ehlo()
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, text)

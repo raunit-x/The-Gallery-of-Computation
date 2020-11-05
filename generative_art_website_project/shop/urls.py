fa17c6a362c5c8e9ebd0782c49aacd3609dae01c
Raunit <raunitd.it.17@nsit.net.in>
2020-11-05 10:31:16 +0530

    footer fixes

--- generative_art_website_project/shop/urls.py
+++ generative_art_website_project/shop/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from . import views
 
@@ -29,4 +28,3 @@ urlpatterns = [
     path('subscribe/', views.subscribe, name='subscribe'),
     path('unsubscribe/', views.unsubscribe, name='unsubscribe'),
 ]
-

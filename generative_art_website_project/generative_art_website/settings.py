0637bbd223c1e0854fefdd5390a6145dcf714093
Yuvraj <yuvrajpuyam@gmail.com>
2021-02-04 13:45:59 +0530

    debug true

--- generative_art_website_project/generative_art_website/settings.py
+++ generative_art_website_project/generative_art_website/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = '(hby&9eiqojaog=@l06$tfvrn#3&t3^ga)vnu^d9!wr3b%05et'
 SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['74a84de6cbc7.ngrok.io', 'localhost', '127.0.0.1','thegalleryofcomputation.herokuapp.com']
 

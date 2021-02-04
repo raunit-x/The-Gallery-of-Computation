4f4fadf75a3c1564201a55f929db2249ac4e3e96
Yuvraj <yuvrajpuyam@gmail.com>
2021-02-04 13:34:50 +0530

    change ins settings.pu

--- generative_art_website_project/generative_art_website/settings.py
+++ generative_art_website_project/generative_art_website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['74a84de6cbc7.ngrok.io', 'localhost', '127.0.0.1','thegalleryofcomputation.herokuapp.com/']
+ALLOWED_HOSTS = ['74a84de6cbc7.ngrok.io', 'localhost', '127.0.0.1','thegalleryofcomputation.herokuapp.com']
 
 
 # Application definition

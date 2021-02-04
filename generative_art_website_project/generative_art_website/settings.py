07c154bde12927dd895f8780714a3f795cfe8bb0
Yuvraj <yuvrajpuyam@gmail.com>
2021-02-04 21:18:52 +0530

    -

--- generative_art_website_project/generative_art_website/settings.py
+++ generative_art_website_project/generative_art_website/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'generative_art_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'shop/Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

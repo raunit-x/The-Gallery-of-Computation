91bc9389d7f2fb6848ae4fd565450e928df047fa
Yuvraj <yuvrajpuyam@gmail.com>
2021-02-04 21:11:01 +0530

    minor fixes

--- generative_art_website_project/generative_art_website/settings.py
+++ generative_art_website_project/generative_art_website/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'generative_art_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

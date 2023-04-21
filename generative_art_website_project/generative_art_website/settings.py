a52361494659c1155e11deadb2b2ea99db057661
Raunit <36741947+raunit-x@users.noreply.github.com>
2023-04-21 11:48:10 +0530

    Update settings.py

--- generative_art_website_project/generative_art_website/settings.py
+++ generative_art_website_project/generative_art_website/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = '(hby&9eiqojaog=@l06$tfvrn#3&t3^ga)vnu^d9!wr3b%05et'
 SECRET_KEY = os.environ.get('SECRET_KEY', "(hby&9eiqojaog=@l06$tfvrn#3&t3^ga)vnu^d9!wr3b%05et")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['74a84de6cbc7.ngrok.io', 'localhost', '127.0.0.1', 'thegalleryofcomputation.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
